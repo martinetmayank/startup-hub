686e51cde22ce95dd64016f912cd56c80278ca82
priyanshujoshilpj <joshipriyanshu85@gmail.com>
2021-03-31 02:06:28 +0530

    Startups update functionality

--- startups/urls.py
+++ startups/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('add/', views.add_startup, name='add'),
+    path('<id>/update/', views.update_startup, name='update'),
 ]
--- startups/views.py
+++ startups/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect
+from startups.forms import AddForm
+from django.shortcuts import get_object_or_404, render, redirect
 from . import models
 
 
@@ -12,6 +13,21 @@ def add_startup(request):
     else:
         form = models.AddStartupForm()
     return render(request, 'add.html', {'form': form})
+
+
+def update_startup(request, id):
+    obj = get_object_or_404(AddForm, id=id)
+    form = models.AddStartupForm(request.POST, instance=obj)
+
+    # if request.method == 'POST':
+    #     new_instance = AddForm
+    #     form = models.AddStartupForm(instance=new_instance, data=request.POST)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        return redirect('home')
+
+    return render(request, 'update.html', {'form': form})
 
 
 def error_404(request, exception):
